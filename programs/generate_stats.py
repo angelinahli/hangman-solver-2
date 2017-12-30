6dace6c4ffb6b84e4c6b7e5b623f0457ce362fbc
Angelina Li <ali6@wellesley.edu>
2017-12-31 07:57:19 +0800

    started generate_stats file

---
+++ programs/generate_stats.py
@@ -0,0 +1,7 @@
+"""
+FILENAME: generate_stats.py
+DESC: Script to generate summary statistics about hangman words.
+"""
+
+from programs.vocabulary import sorted_words
+from programs.hangman import StatsHangman
